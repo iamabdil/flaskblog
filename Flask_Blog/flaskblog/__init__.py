5ac791452bdc6e37667ac852344bedf16c6f569d
iamabdil <51669829+iamabdil@users.noreply.github.com>
2021-05-16 22:41:30 +0100

    initial commit

---
+++ Flask_Blog/flaskblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b612a2d4edbef1c8a21768762b020c17'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from flaskblog import routes
